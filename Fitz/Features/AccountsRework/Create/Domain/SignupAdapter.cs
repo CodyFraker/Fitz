616d8ea272269bdcfcdb93938224380d0fd1618f
CodyFraker <codyfraker95@gmail.com>
2025-01-17 18:01:35 -0500

    oops forgot some files

--- Fitz/Features/AccountsRework/Create/Domain/SignupAdapter.cs
+++ Fitz/Features/AccountsRework/Create/Domain/SignupAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSharpPlus.SlashCommands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Fitz.Features.AccountsRework.Create.Domain
 {
-    internal class SignupAdapter
+    [SlashModuleLifespan(SlashModuleLifespan.Scoped)]
+    public sealed class AccountSlashCommands : ApplicationCommandModule
     {
     }
 }
